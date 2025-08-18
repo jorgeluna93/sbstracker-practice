37d05202526d32e5a6b3b29127f4db88b002b4a7
jorgeluna93 <jluna.dev@icloud.com>
2025-08-18 15:41:30 -0600

    Added arcjet

--- config/env.js
+++ config/env.js
@@ -8,7 +8,15 @@ config({path:`.env.${process.env.NODE_ENV || 'development' }.local`});
     NODE_ENV = Enviroment of the api,
     DB_URI = Location of our MongoDB,
     JWT_SECRET = Our token for keep track of user sessions,
-    JWT_EXPIRES_IN = When the token expires
+    JWT_EXPIRES_IN = When the token expires,
+    ARCJET_KEY = ARCJET api key,
+    ARCJET_ENV = ARCJET environment
 */
 
-export const { PORT,NODE_ENV,DB_URI,JWT_SECRET,JWT_EXPIRES_IN } = process.env;
+export const { 
+    PORT,
+    NODE_ENV,
+    DB_URI,
+    JWT_SECRET,JWT_EXPIRES_IN,
+    ARCJET_KEY,ARCJET_ENV 
+} = process.env;
